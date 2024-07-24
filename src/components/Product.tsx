aee9f167e294c2a1d8d3b8aa260ff1bdf980e614
Chonnakan Chinayothin <chonnakan@odds.team>
2024-07-24 15:14:55 +0700

    prop

---
+++ src/components/Product.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { IProduct } from "./ProductList";
+
+type Props = {
+  product: IProduct;
+  onClick: () => void;
+};
+
+export default function Product({ product, onClick }: Props) {
+  return (
+    <div className="group relative" data-testid="product-item" role="listitem">
+      <div className="pw-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+        <Image
+          data-testid="product-item--image"
+          src={product.imageUrl}
+          width={300}
+          height={300}
+          alt={product.name}
+          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <span data-testid="product-item--name" aria-hidden="true">
+              {product.name}
+            </span>
+          </h3>
+        </div>
+        <p
+          data-testid="product-item--price"
+          className="text-sm font-medium text-gray-900"
+        >
+          ${product.price}
+        </p>
+      </div>
+      <div className="flex space-x-2 justify-center mt-2">
+        <button
+          data-testid="product-item--add-to-cart"
+          className="add-to-cart-button px-6 py-1 transition ease-in duration-200 w-full rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none"
+          onClick={onClick}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
+import Product from "./Product";
 
-interface Product {
+export interface IProduct {
   readonly name: string;
   readonly imageUrl: string;
   readonly price: number;
 }
 
-const Products: Product[] = [
+const Products: IProduct[] = [
   {
     name: "Loose Cropped Jeans (Damaged)",
     imageUrl:
@@ -36,52 +36,10 @@ const ProductList = () => {
       {Products.map((product) => (
         <Product
           key={product.name}
-          name={product.name}
-          imageUrl={product.imageUrl}
-          price={product.price}
+          product={product}
+          onClick={() => window.alert("Add to cart: " + product.name)}
         />
       ))}
-    </div>
-  );
-};
-
-const Product = ({ name, imageUrl, price }: Product) => {
-  return (
-    <div className="group relative" data-testid="product-item" role="listitem">
-      <div className="pw-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-        <Image
-          data-testid="product-item--image"
-          src={imageUrl}
-          width={300}
-          height={300}
-          alt={name}
-          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-        />
-      </div>
-      <div className="mt-4 flex justify-between">
-        <div>
-          <h3 className="text-sm text-gray-700">
-            <span data-testid="product-item--name" aria-hidden="true">
-              {name}
-            </span>
-          </h3>
-        </div>
-        <p
-          data-testid="product-item--price"
-          className="text-sm font-medium text-gray-900"
-        >
-          ${price}
-        </p>
-      </div>
-      <div className="flex space-x-2 justify-center mt-2">
-        <button
-          data-testid="product-item--add-to-cart"
-          className="add-to-cart-button px-6 py-1 transition ease-in duration-200 w-full rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none"
-          onClick={() => window.alert("Add to cart: " + name)}
-        >
-          Add to cart
-        </button>
-      </div>
     </div>
   );
 };
