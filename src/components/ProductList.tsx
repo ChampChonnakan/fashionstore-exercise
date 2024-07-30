f9cf70b5339abbc09e91daecfd4b02b06cb31bee
Chonnakan Chinayothin <chonnakan@odds.team>
2024-07-30 20:38:48 +0700

    add update, delete list

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,13 +2,19 @@ import { Product } from "@/models/product";
 
 type ProductListProps = {
   items: Product[];
+  onRemove?: (name: string) => void;
 };
 
-const ProductList = ({ items }: ProductListProps) => {
+const ProductList = ({ items, onRemove }: ProductListProps) => {
   return (
     <ul className="list-disc list-inside">
       {items.map((item) => (
-        <li key={item.name}>{item.name}</li>
+        <li key={item.name}>
+          {item.name}{" "}
+          <button onClick={() => onRemove!(item.name)}>
+            <span className="text-red-500">x</span>
+          </button>
+        </li>
       ))}
     </ul>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,41 @@
 import ProductList from "@/components/ProductList";
+import { useState } from "react";
 
 export default function Home() {
-  const productName = "";
-  const products = [{ name: "Loose Cropped Jeans (Damaged)" }];
+  const [productName, setProductName] = useState("");
+  const [products, setProducts] = useState([
+    { name: "Loose Cropped Jeans (Damaged)" },
+  ]);
+
+  const removeProduct = (name: string) => {
+    setProducts(products.filter((product) => product.name !== name));
+  };
 
   return (
-    <>
+    <div className="p-2">
       <form
         onSubmit={(event) => {
+          if (!products.some((p) => p.name === productName)) {
+            setProducts([...products, { name: productName }]);
+          }
+
           event.preventDefault();
         }}
       >
         <h1 className="text-2xl font-bold">Add a new product</h1>
         <div>
           <label htmlFor="product-name">Product name:</label>
-          <input className="bg-slate-200" id="product-name" />
+          <input
+            className="bg-slate-200"
+            id="product-name"
+            onChange={(event) => setProductName(event.target.value)}
+          />
           <p data-testid="result">{productName}</p>
         </div>
       </form>
       <hr />
-      <ProductList items={products} />
-    </>
+      <h1 className="text-2xl font-bold">Products: {products.length} items</h1>
+      <ProductList items={products} onRemove={removeProduct} />
+    </div>
   );
 }
